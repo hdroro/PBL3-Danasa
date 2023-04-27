b85ac0bc026326e3ea03e81e06ba4d9c9f28d7e2
hongdiem_roro <mingukmanse673@gmail.com>
2023-04-27 19:35:19 +0700

    DH

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'phuongha35',
+  password: 'sa123',
   database: 'danasa'
 });
 
